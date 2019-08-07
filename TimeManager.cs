2f08f850dde9884082fc875a290b1064b8ad753d
xiaoqiangheye <qiangwei@uw.edu>
2019-08-07 18:27:28 +0800

    Initial commit

---
+++ TimeManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+
+
+
+public class TimeManager:MonoBehaviour
+{
+    public static ArrayList timers;
+    static TimeManager()
+    {
+        timers = new ArrayList();
+    }
+
+    public static Timer setInterval(float interval, Action act)
+    {
+        Timer timer = new Timer(interval, true, act,0);
+        timers.Add(timer);
+        return timer;
+    }
+
+
+    public static Timer setTimeOut(float interval, Action act)
+    {
+        Timer timer = new Timer(interval, false, act,1);
+        timers.Add(timer);
+        return timer;
+    }
+
+    public static Timer setInterval(float interval, Action act, int times)
+    {
+        Timer timer = new Timer(interval,false,act, times);
+        timers.Add(timer);
+        return timer;
+    }
+
+    public static void remove(Timer timer)
+    {
+        timer.deactivate();
+        timers.Remove(timer);
+    }
+
+    void Update()
+    {
+        foreach (Timer timer in timers)
+        {
+            timer.Update();
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
+public class Timer : MonoBehaviour
+{
+
+    private bool isActive;
+    private float passedTime;
+    private float time;
+    private Action func;
+    private bool isRepeatedForever;
+    private int repeatedTimes;
+  
+
+    public Timer(float time, bool isRepeatedForever, Action act, int times)
+    {
+        this.time = time;
+        this.isRepeatedForever = isRepeatedForever;
+        this.func = act;
+        this.isActive = false;
+    }
+
+    public void activate()
+    {
+        isActive = true;
+    }
+
+    public void deactivate()
+    {
+        isActive = false;
+    }
+
+
+   
+     
+    public void Update(){
+        if (isActive)
+        {
+            passedTime += Time.deltaTime;
+            if (passedTime >= time)
+            {
+                if (!isRepeatedForever && repeatedTimes > 0)
+                {
+                    func.Invoke();
+                    repeatedTimes -= 1;
+                    
+                }
+                else if(isRepeatedForever)
+                {
+                    func.Invoke();
+                    passedTime = 0f;
+                }
+                else
+                {
+                    isActive = false;
+                    passedTime = 0f;
+                }
+            }
+        }
+    }
+
+}
